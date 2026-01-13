09dd81ac93477fcf268ec62ba71a7efdb4627fed
s22a0061 <s22a0061@siswa.umk.edu.my>
2026-01-13 16:58:29 +0800

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 # Malaysian TOU Tariff
-# Peak: 8:00 AM - 10:00 PM (0.35 RM) | Off-Peak: 10:00 PM - 8:00 AM (0.20 RM)
+# Peak: 8:00 AM - 10:00 PM (0.35 RM)
+# Off-Peak: 10:00 PM - 8:00 AM (0.20 RM)
 TARIFF = [0.20]*8 + [0.35]*14 + [0.20]*2
 
 def calculate_metrics(particle_position, fixed_tasks, shiftable_tasks):
